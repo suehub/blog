d35fdc5a27cb02fe38a260b4b63a9e3183ec3a69
sue <dldustn2040@gmail.com>
2022-11-19 19:44:04 +0900

    jsx

--- src/App.js
+++ src/App.js
@@ -2,22 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let posts = { color : 'blue', fontSize : '30px'}
+  function 함수(){
+    return 100
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          안녕하세요.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div style={ posts }>개발 Blog</div>
+      </div>
+      <img src={logo}></img>
+      <h4>{ posts }</h4>
     </div>
   );
 }
