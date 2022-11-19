860cff52d8c5a286868f1f8d29261680320d9dc2
sue <dldustn2040@gmail.com>
2022-11-19 19:46:08 +0900

    +

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import './App.css';
 function App() {
 
   let posts = { color : 'blue', fontSize : '30px'}
-  function 함수(){
-    return 100
-  }
 
   return (
     <div className="App">
@@ -14,7 +11,6 @@ function App() {
         <div style={ posts }>개발 Blog</div>
       </div>
       <img src={logo}></img>
-      <h4>{ posts }</h4>
     </div>
   );
 }
