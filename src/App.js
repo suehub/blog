0e9ae2bdcaab055114b7081692958e8eb63ef7a6
sue <dldustn2040@gmail.com>
2022-11-19 20:17:50 +0900

    state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
-  let posts = { color : 'blue', fontSize : '30px'}
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집']);
+
+  let posts = '강남 고기 맛집'; // 변수에 데이터 저장
 
   return (
     <div className="App">
       <div className="black-nav">
-        <div style={ posts }>개발 Blog</div>
+        <div>개발 Blog</div>
       </div>
-      <img src={logo}></img>
+      <div className='list'>
+        <h3> { 글제목[1] } </h3>  {/* 데이터바인딩 */}
+        <p>11월 19일 발행</p>
+        <hr/>
+      </div>
     </div>
   );
 }
