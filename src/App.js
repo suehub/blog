353188df3bf7d067ad0735c09e682bfe7f9011a0
sue <dldustn2040@gmail.com>
2022-11-20 21:33:55 +0900

    state_homework

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ function App() {
   let [글제목, 글제목변경] = useState(['롱 코트 추천', '강남 우동 맛집', '겨울 목도리 추천', '일산 맛집 정리']);
   let [따봉, 따봉변경] = useState(0);
   let posts = '강남 고기 맛집'; 
+  
+  function 제목변경(){
+    var newArray = [...글제목];
+    newArray[0] = '롱 패딩 추천';
+    글제목변경(newArray);
+  }
 
   return (
     <div className="App">
@@ -17,6 +23,7 @@ function App() {
       <div className='list'>
         <h3> { 글제목[0] } <span onClick={ ()=>{ 따봉변경(따봉 + 1)}}>👍</span> { 따봉 } </h3> 
         <p>11월 10일 발행</p>
+        <button onClick={  제목변경 }>버튼</button>
         <hr/>
       </div>
       <div className='list'>
