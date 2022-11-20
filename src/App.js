490c005f6d156855534cbdfac46b771ba00dc77e
sue <dldustn2040@gmail.com>
2022-11-20 20:38:27 +0900

    event

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
+/* eslint-disable */
 import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
-  let [글제목, 글제목변경] = useState('남성 코트 추천');
-  let [글제목2, 글제목변경2] = useState('강남 우동 맛집');
-  let [글제목3, 글제목변경3] = useState('겨울 목도리 추천');
-  let [글제목4, 글제목변경4] = useState('일산 맛집 정리');
-
-
-  let posts = '강남 고기 맛집'; // 변수에 데이터 저장
+  let [글제목, 글제목변경] = useState(['롱 코트 추천', '강남 우동 맛집', '겨울 목도리 추천', '일산 맛집 정리']);
+  let [따봉, 따봉변경] = useState(0);
+  let posts = '강남 고기 맛집'; 
 
   return (
     <div className="App">
@@ -18,24 +15,24 @@ function App() {
         <div>개발 Blog</div>
       </div>
       <div className='list'>
-        <h3> { 글제목 } </h3>  {/* 데이터바인딩 */}
+        <h3> { 글제목[0] } <span onClick={ ()=>{ 따봉변경(따봉 + 1)}}>👍</span> { 따봉 } </h3> 
         <p>11월 10일 발행</p>
         <hr/>
       </div>
-      <div className='list2'>
-        <h3> { 글제목2 } </h3>  
+      <div className='list'>
+        <h3> { 글제목[1] } </h3>  
         <p>11월 12일 발행</p>
-        <hr className='hr'/>
+        <hr/>
       </div>
-      <div className='list2'>
-        <h3> { 글제목3 } </h3>  
+      <div className='list'>
+        <h3> { 글제목[2] } </h3>  
         <p>11월 15일 발행</p>
-        <hr className='hr'/>
+        <hr/>
       </div>
-      <div className='list2'>
-        <h3> { 글제목4 } </h3>  
+      <div className='list'>
+        <h3> { 글제목[3] } </h3>  
         <p>11월 19일 발행</p>
-        <hr className='hr'/>
+        <hr/>
       </div>
     </div>
   );
