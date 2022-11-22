7137e1bff70e2c02b474d7f2cd6b796d8bfe8ec0
sue <dldustn2040@gmail.com>
2022-11-22 23:52:55 +0900

    component

--- src/App.js
+++ src/App.js
@@ -41,8 +41,23 @@ function App() {
         <p>11월 19일 발행</p>
         <hr/>
       </div>
+
+      <Modal />
+
     </div>
   );
 }
+
+function Modal(){
+  return (
+    <div className='modal'>
+        <h2>제목</h2>
+        <p>날짜</p>
+        <p>상세내용</p>
+    </div>
+  )
+}
+
+
 
 export default App;
